6146b9bf3478fedf08087f5af49ee29279e175ca
karan <qcnstant@gmail.com>
2018-11-19 20:29:12 +0530

    init chess grid and display pieces

--- script/basepiece.js
+++ script/basepiece.js
@@ -68,8 +68,8 @@ for(i=0 ; i<8 ; i++)
     var white_pawn_temp_id = 'wp' + index;
     var black_pawn_temp_id = 'bp' + index; 
 
-    pawn_white.push(new pawn('white', null, null, 1, white_pawn_temp_id));
-    pawn_black.push(new pawn('black', null, null, 1, black_pawn_temp_id));
+    pawn_white.push(new pawn('white', null, '<img width="30%" src="./images/wp.svg"></img>', 1, white_pawn_temp_id));
+    pawn_black.push(new pawn('black', null, '<img width="30%" src="./images/bp.svg"></img>', 1, black_pawn_temp_id));
 
     Object.preventExtensions(pawn_white[i]);
     Object.preventExtensions(pawn_black[i]);
@@ -81,33 +81,33 @@ for(i=0; i<2; i++)
 
     var white_rook_temp_id = 'wr' + index;
     var black_rook_temp_id = 'br' + index;
-    rook_white.push(new rook('white', null, null, 5, white_rook_temp_id));
-    rook_black.push(new rook('white', null, null, 5, black_rook_temp_id));
+    rook_white.push(new rook('white', null, '<img width="30%" src="./images/wr.svg"></img>', 5, white_rook_temp_id));
+    rook_black.push(new rook('black', null, '<img width="30%" src="./images/br.svg"></img>', 5, black_rook_temp_id));
     Object.preventExtensions(rook_white[i]);
     Object.preventExtensions(rook_black[i]);
 
     var white_knight_temp_id = 'wk' + index;
     var black_knight_temp_id = 'bk' + index;
-    knight_white.push(new rook('white', null, null, 3, white_knight_temp_id));
-    knight_black.push(new rook('white', null, null, 3, black_knight_temp_id));
+    knight_white.push(new rook('white', null, '<img width="30%" src="./images/wk.svg"></img>', 3, white_knight_temp_id));
+    knight_black.push(new rook('black', null, '<img width="30%" src="./images/bk.svg"></img>', 3, black_knight_temp_id));
     Object.preventExtensions(knight_white[i]);
     Object.preventExtensions(knight_black[i]);
 
     var white_bishop_temp_id = 'wb' + index;
     var black_bishop_temp_id = 'bb' + index;
-    bishop_white.push(new rook('white', null, null, 3, white_bishop_temp_id));
-    bishop_black.push(new rook('white', null, null, 3, black_bishop_temp_id));
+    bishop_white.push(new rook('white', null, '<img width="30%" src="./images/wb.svg"></img>', 3, white_bishop_temp_id));
+    bishop_black.push(new rook('black', null, '<img width="30%" src="./images/bb.svg"></img>', 3, black_bishop_temp_id));
     Object.preventExtensions(bishop_white[i]);
     Object.preventExtensions(bishop_black[i]);
 }
 
-king_white = new king('white', null, null, Infinity, 'wv');
-king_black = new king('black', null, null, Infinity, 'bv');
+king_white = new king('white', null, '<img width="30%" src="./images/wv.svg"></img>', Infinity, 'wv');
+king_black = new king('black', null, '<img width="30%" src="./images/bv.svg"></img>', Infinity, 'bv');
 Object.preventExtensions(king_white);
 Object.preventExtensions(king_black);
 
-queen_white = new queen('white', null, null, 9, 'wx');
-queen_black = new queen('black', null, null, 9, 'bx');
+queen_white = new queen('white', null, '<img width="30%" src="./images/wx.svg"></img>', 9, 'wx');
+queen_black = new queen('black', null, '<img width="30%" src="./images/bx.svg"></img>', 9, 'bx');
 Object.preventExtensions(queen_white);
 Object.preventExtensions(queen_black);
 
@@ -139,52 +139,52 @@ function initPositions() {
 
     //Init Pawn postions
     for(i=0; i<8; i++) {
-        chess_board[1][i]=pawn_black[i].id;
+        chess_board[1][i]=pawn_black[i].image;
         pawn_black[i].position=[1,i];
-        chess_board[6][i]=pawn_white[i].id;
+        chess_board[6][i]=pawn_white[i].image;
         pawn_white[i].position=[6,i];
     }
 
     //Init Rooks
-    chess_board[0][0] = rook_black[0].id;
-    chess_board[0][7] = rook_black[1].id;
+    chess_board[0][0] = rook_black[0].image;
+    chess_board[0][7] = rook_black[1].image;
     rook_black[0].position = [0,0];
     rook_black[1].position = [0,7];
-    chess_board[7][0] = rook_white[0].id;
-    chess_board[7][7] = rook_white[1].id;
+    chess_board[7][0] = rook_white[0].image;
+    chess_board[7][7] = rook_white[1].image;
     rook_white[0].position = [7,0];
     rook_white[1].position = [7,7];
 
     //Init knights
-    chess_board[0][1] = knight_black[0].id;
-    chess_board[0][6] = knight_black[1].id;
+    chess_board[0][1] = knight_black[0].image;
+    chess_board[0][6] = knight_black[1].image;
     knight_black[0].position = [0,1];
     knight_black[1].position = [0,6];
-    chess_board[7][1] = knight_white[0].id;
-    chess_board[7][6] = knight_white[1].id;
+    chess_board[7][1] = knight_white[0].image;
+    chess_board[7][6] = knight_white[1].image;
     knight_black[0].position = [7,1];
     knight_black[1].position = [7,6];
 
     //Init bishops
-    chess_board[0][2] = bishop_black[0].id;
-    chess_board[0][5] = bishop_black[1].id;
+    chess_board[0][2] = bishop_black[0].image;
+    chess_board[0][5] = bishop_black[1].image;
     bishop_black[0].position = [0,2];
     bishop_black[1].position = [0,5];
-    chess_board[7][2] = bishop_white[0].id;
-    chess_board[7][5] = bishop_white[1].id;
+    chess_board[7][2] = bishop_white[0].image;
+    chess_board[7][5] = bishop_white[1].image;
     bishop_black[0].position = [7,2];
     bishop_black[1].position = [7,5];
 
     //Init kings
-    chess_board[0][3] = king_black.id;
+    chess_board[0][3] = king_black.image;
     king_black.position = [0,3];
-    chess_board[7][3] = king_white.id;
+    chess_board[7][3] = king_white.image;
     king_white.position = [0,3];
     
     //Init queens
-    chess_board[0][4] = queen_black.id;
+    chess_board[0][4] = queen_black.image;
     queen_black.position = [0,4];
-    chess_board[7][4] = queen_white.id;
+    chess_board[7][4] = queen_white.image;
     queen_white.position = [0,4];
 }
 
@@ -198,9 +198,11 @@ function display() {
         var row = table.insertRow(i);
         for(j=0; j<8; j++) {
             var cell = row.insertCell(j);
-            cell.innerHTML = '<img src="./images/bb.svg"> </img>';
+            cell.innerHTML = chess_board[i][j];
         }
     }
+
+    console.log(chess_board[0][0]);
     
     // var p = document.getElementById("test");
     // p.innerHTML = 
