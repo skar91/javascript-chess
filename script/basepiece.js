0b9597b8ae03dade4dc14bd6be0123e68973e88f
Mankaran Bedi <mankaran@cctech.co.in>
2018-11-19 11:43:07 +0530

    added few testcases

--- script/basepiece.js
+++ script/basepiece.js
@@ -70,6 +70,9 @@ for(i=0 ; i<8 ; i++)
 
     pawn_white.push(new pawn('white', null, null, 1, white_pawn_temp_id));
     pawn_black.push(new pawn('black', null, null, 1, black_pawn_temp_id));
+
+    Object.preventExtensions(pawn_white[i]);
+    Object.preventExtensions(pawn_black[i]);
 }
 
 for(i=0; i<2; i++)
@@ -80,23 +83,53 @@ for(i=0; i<2; i++)
     var black_rook_temp_id = 'br' + index;
     rook_white.push(new rook('white', null, null, 5, white_rook_temp_id));
     rook_black.push(new rook('white', null, null, 5, black_rook_temp_id));
+    Object.preventExtensions(rook_white[i]);
+    Object.preventExtensions(rook_black[i]);
 
     var white_knight_temp_id = 'wk' + index;
     var black_knight_temp_id = 'bk' + index;
     knight_white.push(new rook('white', null, null, 3, white_knight_temp_id));
     knight_black.push(new rook('white', null, null, 3, black_knight_temp_id));
+    Object.preventExtensions(knight_white[i]);
+    Object.preventExtensions(knight_black[i]);
 
     var white_bishop_temp_id = 'wb' + index;
     var black_bishop_temp_id = 'bb' + index;
     bishop_white.push(new rook('white', null, null, 3, white_bishop_temp_id));
     bishop_black.push(new rook('white', null, null, 3, black_bishop_temp_id));
+    Object.preventExtensions(bishop_white[i]);
+    Object.preventExtensions(bishop_black[i]);
 }
 
 king_white = new king('white', null, null, Infinity, 'wv');
 king_black = new king('black', null, null, Infinity, 'bv');
+Object.preventExtensions(king_white);
+Object.preventExtensions(king_black);
 
 queen_white = new queen('white', null, null, 9, 'wx');
 queen_black = new queen('black', null, null, 9, 'bx');
+Object.preventExtensions(queen_white);
+Object.preventExtensions(queen_black);
+
+
+function assert(condition) {
+    if(!condition) {
+        console.log("assertion failed!")
+    }
+    else {
+        console.log("success");
+    }
+}
+
+function testcases() {
+    console.log("testing extensibility...");
+    for(i=0; i<8; i++) {
+        assert(Object.isExtensible(pawn_white[i]));
+        assert(Object.isExtensible(pawn_black[i]));
+    }
+    //assert(pawn_white[0].color == "white");
+}
+
 
 function display() {
     pawn_white[0].movement();
