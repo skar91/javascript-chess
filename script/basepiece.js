7bf96c07c4d22cd334e8791cf600f10116ee395b
Mankaran Bedi <mankaran@cctech.co.in>
2018-11-27 14:31:59 +0530

    Massive changes. Now requires only one object of each piece. Corrected movement rules of all pieces.

--- script/basepiece.js
+++ script/basepiece.js
@@ -1,34 +1,30 @@
 class BasePiece {
-    constructor(color,position,image,point,id) {
-        this.color = color;
-        this.position = position;
-        this.image = image;
-        this.point = point;
+    constructor(id) {
         this.id = id; 
     }
 }
-
-const COLOR = {
-    white: 'white',
-    black: 'black'
+///////////////////////// GLOBAL VARIABLES ////////////////////////////////////////////////////////
+const PIECE_COLOR = {
+    white: 'w',
+    black: 'b'
 }
 
 const IMAGE_SOURCE_TAG = {
-    whitepawn: '<img src="./images/wp.svg"></img>',
-    blackpawn: '<img src="./images/bp.svg"></img>',
-    whitebishop: '<img src="./images/wb.svg"></img>',
-    blackbishop: '<img src="./images/bb.svg"></img>',
-    whiterook: '<img src="./images/wr.svg"></img>',
-    blackrook: '<img src="./images/br.svg"></img>',
-    whiteknight: '<img src="./images/wk.svg"></img>',
-    blackknight: '<img src="./images/bk.svg"></img>',
-    whiteking: '<img src="./images/wx.svg"></img>',
-    blackking: '<img src="./images/bx.svg"></img>',
-    whitequeen: '<img src="./images/wq.svg"></img>',
-    blackqueen: '<img src="./images/bq.svg"></img>',  
+    wp: '<img src="./images/wp.svg"></img>',
+    bp: '<img src="./images/bp.svg"></img>',
+    wb: '<img src="./images/wb.svg"></img>',
+    bb: '<img src="./images/bb.svg"></img>',
+    wr: '<img src="./images/wr.svg"></img>',
+    br: '<img src="./images/br.svg"></img>',
+    wk: '<img src="./images/wk.svg"></img>',
+    bk: '<img src="./images/bk.svg"></img>',
+    wx: '<img src="./images/wx.svg"></img>',
+    bx: '<img src="./images/bx.svg"></img>',
+    wq: '<img src="./images/wq.svg"></img>',
+    bq: '<img src="./images/bq.svg"></img>',  
 }
 
-const PointsTable = {
+const POINTS_TABLE = {
     pawn_point: 1,
     rook_point: 5,
     knight_point: 3,
@@ -37,41 +33,47 @@ const PointsTable = {
     king_point: Infinity 
 }
 
+var piece = {};
+var validcoordinates;
+var chess_board;
+var player_turn = 'w';
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
 class pawn extends BasePiece {
-    constructor(color,position,image,point,id) {
-        super(color,position,image,point,id);
+    constructor(id) {
+        super(id);
     }
 
-    validMoves() {
-        var validcoordinates =[];
-        var x = this.position[0];
-        var y = this.position[1];
-        if(this.id=='wp') {
-            if(x==6 && chess_board[4][y-2][0]!='w') {
+    validMoves(current_position,color) {
+        validcoordinates = [];
+        let x = current_position[0];
+        let y = current_position[1];
+        if(color=='w') {
+            if(x==6 && chess_board[4][y][0]!='w') {
                  validcoordinates.push([4][y-2]);
             }
             if(chess_board[x-1][y-1][0]=='b') {
                 validcoordinates.push([x-1,y-1]);
             }
-            if(chess_board[x+1][y-1][0]=='b') {
+            if(chess_board[x-1][y+1][0]=='b') {
                 validcoordinates.push([x+1][y-1]);
             }
-            if(chess_board[x][y-1]!='w' && chess_board[x][y-1]!='b') {
+            if(chess_board[x-1][y]!='w' && chess_board[x-1][y]!='b') {
                 validcoordinates.push([x,y-1]);
             }
         }
         else {
-           if(x==1 && chess_board[3][y+2][0]!='b') {
-                validcoordinates.push([3][y+2]);
+           if(x==1 && chess_board[3][y][0]!='b') {
+                validcoordinates.push([3][y]);
+           }
+           if(chess_board[x+1][y-1][0]=='w') {
+               validcoordinates.push([x+1,y-1]);
            }
            if(chess_board[x+1][y+1][0]=='w') {
-               validcoordinates.push([x+1,y+1]);
+               validcoordinates.push([x+1][y+1]);
            }
-           if(chess_board[x-1][y+1][0]=='w') {
-               validcoordinates.push([x-1][y+1]);
-           }
-           if(chess_board[x][y+1]!='w' && chess_board[x][y+1]!='b') {
-               validcoordinates.push([x,y+1]);
+           if(chess_board[x+1][y]!='w' && chess_board[x+1][y]!='b') {
+               validcoordinates.push([x+1,y]);
            }
         }
         return validcoordinates;
@@ -79,39 +81,18 @@ class pawn extends BasePiece {
 }
 
 class rook extends BasePiece {
-    constructor(color,position,image,point,id) {
-        super(color,position,image,point,id);
+    constructor(id) {
+        super(id);
     }
-    validMoves() {
-        var validcoordinates = [];
-        var x = this.position[0];
-        var y = this.position[1];
-        opp_color = this.color=='white' ? 'b' : 'w';
+    validMoves(current_position,color) {
+        validcoordinates = [];
+        let x = current_position[0];
+        let y = current_position[1];
+        let i = 0;
+        opp_color = color=='w' ? 'b' : 'w';
         /*Right*/
-        for(i = x + 1 ; i < 8 ; i++) {
-            if(chess_board[i][y][0]==this.color[0]) {
-                break;
-            }
-            else if(chess_board[i][y][0]==opp_color) {
-                validcoordinates.push([i,y]);
-                break;
-            }
-            validcoordinates.push([i,y]);
-        }
-        /*Left*/
-        for(i = x - 1 ; i > -1 ; i--) {
-            if(chess_board[i][y][0]==this.color[0]) {
-                break;
-            }
-            else if(chess_board[i][y][0]==opp_color) {
-                validcoordinates.push([i,y]);
-                break;
-            }
-            validcoordinates.push([i,y]);
-        }
-        /*Down*/
         for(i = y + 1 ; i < 8 ; i++) {
-            if(chess_board[x][i][0]==this.color[0]) {
+            if(chess_board[x][i][0]==color) {
                 break;
             }
             else if(chess_board[x][i][0]==opp_color) {
@@ -120,9 +101,186 @@ class rook extends BasePiece {
             }
             validcoordinates.push([x,i]);
         }
-        /*Up*/
+        /*Left*/
         for(i = y - 1 ; i > -1 ; i--) {
-            if(chess_board[x][i][0]==this.color[0]) {
+            if(chess_board[x][i][0]==color) {
+                break;
+            }
+            else if(chess_board[x][i][0]==opp_color) {
+                validcoordinates.push([x,i]);
+                break;
+            }
+            validcoordinates.push([x,i]);
+        }
+        /*Down*/
+        for(i = x + 1 ; i < 8 ; i++) {
+            if(chess_board[i][y][0]==color) {
+                break;
+            }
+            else if(chess_board[i][y][0]==opp_color) {
+                validcoordinates.push([i,y]);
+                break;
+            }
+            validcoordinates.push([i,y]);
+        }
+        /*Up*/
+        for(i = x - 1 ; i > -1 ; i--) {
+            if(chess_board[i][y][0]==color) {
+                break;
+            }
+            else if(chess_board[i][y][0]==opp_color) {
+                validcoordinates.push([i,y]);
+                break;
+            }
+            validcoordinates.push([i,y]);
+        }
+        return validcoordinates;
+    }
+}
+
+class knight extends BasePiece {
+    constructor(id) {
+        super(id);
+    }
+    validMoves(current_position,color) {
+        validcoordinates = [];
+        let x = current_position[0];
+        let y = current_position[1];
+        opp_color = color=='w' ? 'b' : 'w';
+
+        /*Top*/
+        if(x>=2) {
+            if(y>=1 && y<=6) {
+                if(chess_board[x-2][y-1][0]!=color) {
+                    validcoordinates.push([x-2,y-1]);
+                }
+                if(chess_board[x-2][y+1][0]!=color) {
+                    validcoordinates.push([x-2,y+1]);
+                }
+            }
+            if(y==0) {
+                if(chess_board[x-2][y+1][0]!=color) {
+                    validcoordinates.push([x-2,y+1]);
+                }
+            }
+            if(y==7) {
+                if(chess_board[x-2][y-1][0]!=color) {
+                    validcoordinates.push([x-2,y-1]);
+                }
+            }
+        }
+        /*Bottom*/
+        if(x<=5) {
+            if(y>=1 && y<=6) {
+                if(chess_board[x+2][y-1][0]!=color) {
+                    validcoordinates.push([x+2,y-1]);
+                }
+                if(chess_board[x+2][y+1][0]!=color) {
+                    validcoordinates.push([x+2,y+1]);
+                }
+            }
+            if(y==0) {
+                if(chess_board[x+2][y+1][0]!=color) {
+                    validcoordinates.push([x+2,y+1]);
+                }
+            }
+            if(y==7) {
+                if(chess_board[x+2][y-1][0]!=color) {
+                    validcoordinates.push([x+2,y-1]);
+                }
+            }
+        }
+        /*Left*/
+        if(y>=2) {
+            if(x>=1 && x<=6) {
+                if(chess_board[x-1][y-2][0]!=color) {
+                    validcoordinates.push([x-1,y-2]);
+                }
+                if(chess_board[x+1][y-2][0]!=color) {
+                    validcoordinates.push([x+1,y-2]);
+                }
+            }
+            if(x==0) {
+                if(chess_board[x+1][y-2][0]!=color) {
+                    validcoordinates.push([x+1,y-2]);
+                }
+            }
+            if(x==7) {
+                if(chess_board[x-1][y-2][0]!=color) {
+                    validcoordinates.push([x-1,y-2]);
+                }
+            }
+        }
+        /*Right*/
+        if(y>=5) {
+            if(x>=1 && x<=6) {
+                if(chess_board[x-1][y+2][0]!=color) {
+                    validcoordinates.push([x-1,y+2]);
+                }
+                if(chess_board[x+1][y+2][0]!=color) {
+                    validcoordinates.push([x+1,y+2]);
+                }
+            }
+            if(x==0) {
+                if(chess_board[x+1][y+2][0]!=color) {
+                    validcoordinates.push([x+1,y+2]);
+                }
+            }
+            if(x==7) {
+                if(chess_board[x-1][y+2][0]!=color) {
+                    validcoordinates.push([x-1,y+2]);
+                }
+            }
+        }
+        return validcoordinates;
+    }
+}
+
+class bishop extends BasePiece {
+    constructor(id) {
+        super(id);
+    }
+    validMoves(current_position,color) {
+        validcoordinates = [];
+        let x = current_position[0];
+        let y = current_position[1];
+        opp_color = color=='w' ? 'b' : 'w';
+        /*Bottom-Left*/
+        for(i = x + 1, j = y - 1 ; i < 8 && j >= 0 ; i++ , j--) {
+            if(chess_board[i][y][0]==color) {
+                break;
+            }
+            else if(chess_board[i][y][0]==opp_color) {
+                validcoordinates.push([i,y]);
+                break;
+            }
+            validcoordinates.push([i,y]);
+        }
+        /*Top-Left*/
+        for(i = x - 1, j = y - 1 ; i >= 0 && j >= 0 ; i-- , j--) {
+            if(chess_board[i][y][0]==color) {
+                break;
+            }
+            else if(chess_board[i][y][0]==opp_color) {
+                validcoordinates.push([i,y]);
+                break;
+            }
+            validcoordinates.push([i,y]);
+        }
+        /*Bottom-Right*/
+        for(i = x + 1, j = y + 1 ; i < 8 && j < 8 ; i++ , j++) {
+            if(chess_board[x][i][0]==color) {
+                break;
+            }
+            else if(chess_board[x][i][0]==opp_color) {
+                validcoordinates.push([x,i]);
+                break;
+            }
+            validcoordinates.push([x,i]);
+        }
+        /*Top-Right*/
+        for(i = x - 1, j = y + 1 ; i >= 0 && j < 8 ; i-- , j++) {
+            if(chess_board[x][i][0]==color) {
                 break;
             }
             else if(chess_board[x][i][0]==opp_color) {
@@ -135,87 +293,74 @@ class rook extends BasePiece {
     }
 }
 
-class knight extends BasePiece {
-    constructor(color,position,image,point,id) {
-        super(color,position,image,point,id);
-    }
-}
-
-class bishop extends BasePiece {
-    constructor(color,position,image,point,id) {
-        super(color,position,image,point,id);
-    }
-}
-
 class king extends BasePiece {
-    constructor(color,position,image,point,id) {
-        super(color,position,image,point,id);
+    validMoves(current_position,color) {
+        validcoordinates = [];
+        let x = current_position[0];
+        let y = current_position[1];
+        opp_color = color=='w' ? 'b' : 'w';
+        
+        if((x-1) >= 0 && (y-1) >= 0) validcoordinates.push([x-1,y-1]);
+        if((x-1) >= 0) validcoordinates.push([x-1,y]);
+        if((x-1) >= 0 && (y+1) < 8) validcoordinates.push([x-1,y+1]);
+        if((y-1) >= 0) validcoordinates.push([x,y-1]);
+        if((y+1) < 8) validcoordinates.push([x,y+1]);
+        if((x+1) < 8 && (y-1) >= 0) validcoordinates.push([x+1,y-1]);
+        if((x+1) < 8) validcoordinates.push([x+1,y]);
+        if((x+1) < 8 && (y+1) < 8) validcoordinates.push([x+1,y+1]);    
+        
+        return validcoordinates;
     }
 }
 
 class queen extends BasePiece {
-    constructor(color,position,image,point,id) {
-        super(color,position,image,point,id);
+    constructor(id) {
+        super(id);
+    }
+    validMoves(current_position,color) {
+        validcoordinates = [];
+        let x = current_position[0];
+        let y = current_position[1];
+        opp_color = color=='w' ? 'b' : 'w';
+
+        let temp_rook = new rook('temprook');
+        let temp_bishop = new bishop('tempbishop');
+
+        let validcoordinates1 = temp_rook.validMoves([x,y],color);
+        let validcoordinates2 = temp_bishop.validMoves([x,y],color);
+
+        validcoordinates1.forEach(function(element) {
+            validcoordinates.push(element);
+        });
+        validcoordinates2.forEach(function(element) {
+            validcoordinates.push(element);
+        });
+        return validcoordinates;
+
     }
 }
-
-const fun = () => () => {
-
-}
-
-var white_pieces = [];
-var black_pieces = [];
 
 function initPieces() {
-    for(i=0 ; i<8 ; i++) {
-        white_pieces.push(new pawn(COLOR.white, [6,i], IMAGE_SOURCE_TAG.whitepawn, PointsTable.pawn_point, 'wp'));
-        black_pieces.push(new pawn(COLOR.black, [1,i], IMAGE_SOURCE_TAG.blackpawn, PointsTable.pawn_point, 'bp'));
-    }
-
-    for(i=0; i<8; i+=7) {
-        white_pieces.push(new rook(COLOR.white, [7,i], IMAGE_SOURCE_TAG.whiterook, PointsTable.rook_point, 'wr'));
-        black_pieces.push(new rook(COLOR.black, [0,i], IMAGE_SOURCE_TAG.blackrook, PointsTable.rook_point, 'br'));     
-    }
-    for(i=1; i<7; i+=5) {
-        white_pieces.push(new knight(COLOR.white, [7,i], IMAGE_SOURCE_TAG.whiteknight, PointsTable.knight_point, 'wk'));
-        black_pieces.push(new knight(COLOR.black, [0,i], IMAGE_SOURCE_TAG.blackknight, PointsTable.knight_point, 'bk'));      
-    }
-    for(i=2; i<8; i+=3) {
-        white_pieces.push(new bishop(COLOR.white, [7,i], IMAGE_SOURCE_TAG.whitebishop, PointsTable.bishop_point, 'wb'));
-        black_pieces.push(new bishop(COLOR.black, [0,i], IMAGE_SOURCE_TAG.blackbishop, PointsTable.bishop_point, 'bb'));   
-    }
-
-    white_pieces.push(new king(COLOR.white, [7,3], IMAGE_SOURCE_TAG.whiteking, PointsTable.king_point, 'wx'));
-    black_pieces.push(new king(COLOR.black, [0,3], IMAGE_SOURCE_TAG.blackking, PointsTable.king_point, 'bx'));
     
-    white_pieces.push(new queen(COLOR.white, [7,4], IMAGE_SOURCE_TAG.whitequeen, PointsTable.queen_point, 'wq'));
-    black_pieces.push(new queen(COLOR.black, [0,4], IMAGE_SOURCE_TAG.blackqueen, PointsTable.queen_point, 'bq'));
+    var p = new pawn('p');
+    var r = new rook('r');
+    var k = new knight('k');
+    var b = new bishop('b');
+    var x = new king('x');
+    var q = new queen('q'); 
 
-    /* Enforce rules on piece objects*/
-    white_pieces.forEach(function(element) {
-        Object.preventExtensions(element);
-    });
-    black_pieces.forEach(function(element) {
-        Object.preventExtensions(element);
-    })
-}
-
-/* 
-Test cases
-*/
-function testcases() {
-    console.log("testing extensibility...");
-    const extensibleErrorMsg = "object is not extensible";
-    for(i=0; i<8; i++) {
-        console.assert(Object.isExtensible(white_pieces[i]), {object: white_pieces[i], error: extensibleErrorMsg});
-        console.assert(Object.isExtensible(black_pieces[i]), {object: black_pieces[i], error: extensibleErrorMsg});
-    }
+    piece['p'] = p;
+    piece['r'] = r;
+    piece['k'] = k;
+    piece['b'] = b;
+    piece['x'] = x;
+    piece['q'] = q;
 }
 
 /*
 Chess board is a 2D array of 8x8.
 The value of a square represents if its empty or occupied.
-For eg: position [3,4] = 0 =>  empty
+For eg: position [3,4] = 00 =>  empty
         position [5,6] = 'bk' => black knight
 */
 function initBoard() {
@@ -226,21 +371,43 @@ function initBoard() {
 
     for(i=0; i<8; i++) {
         for(j=0; j<8; j++) {
-            chess_board[i][j]=0;
+            chess_board[i][j]='00';
         }
     }
-    white_pieces.forEach(function(element){
-        var pos_x = element.position[0];
-        var pos_y = element.position[1];
-        chess_board[pos_x][pos_y] = element.id;
-        
-    });
-    black_pieces.forEach(function(element){
-        var pos_x = element.position[0];
-        var pos_y = element.position[1];
-        chess_board[pos_x][pos_y] = element.id;
-        
-    });
+    for(i=0;i<8;i++) {
+        chess_board[1][i] = 'bp';
+        chess_board[6][i] = 'wp';
+    }
+    for(i=0;i<8;i+=7) {
+        chess_board[0][i] = 'br';
+        chess_board[7][i] = 'wr';
+    }
+    for(i=1;i<=6;i+=5) {
+        chess_board[0][i] = 'bk';
+        chess_board[7][i] = 'wk';
+    }
+    for(i=2;i<=5;i+=3) {
+        chess_board[0][i] = 'bb';
+        chess_board[7][i] = 'wb';
+    }
+    chess_board[0][3] = 'bx';
+    chess_board[0][4] = 'bq';
+    chess_board[7][3] = 'wx';
+    chess_board[7][4] = 'wq';
+
+}
+
+
+function dragStart() {
+    var x = this.parentNode.parentNode.rowIndex;
+    var y = this.parentNode.cellIndex;
+    
+    console.log(x);
+    console.log(y);
+}
+
+function dragEnd() {
+    console.log("drag ended");
 }
 
 function display() {
@@ -253,6 +420,10 @@ function display() {
         var row = table.insertRow(i);
         for(j=0; j<8; j++) {
             var cell = row.insertCell(j);
+            if(chess_board[i][j]!='00') {
+                cell.innerHTML = IMAGE_SOURCE_TAG[chess_board[i][j]];
+            }
+            //cell.setAttribute("draggable",true);
             if(i%2==0) {
                 if(j%2==0) {
                     cell.classList.add("blackcell");
@@ -265,15 +436,12 @@ function display() {
             }
         }
     }
-    white_pieces.forEach(function(element) {
-        var pos_x = element.position[0];
-        var pos_y = element.position[1];
-        table.rows[pos_x].cells[pos_y].innerHTML = element.image;
-    });
-    black_pieces.forEach(function(element) {
-        var pos_x = element.position[0];
-        var pos_y = element.position[1];
-        table.rows[pos_x].cells[pos_y].innerHTML = element.image;
-    });
+    imgs = document.getElementsByTagName('img');
+    //console.log(imgs);
+    for(i=0;i<imgs.length;i++)
+    {
+        imgs[i].addEventListener('dragstart', dragStart);
+        imgs[i].addEventListener('dragend', dragEnd);
+    }
 }
 
